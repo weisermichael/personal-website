b343c6d870d5400163f9c4e28866e8f5adfc5119
Michael Weiser <weiserjmichael@gmail.com>
2026-01-23 13:58:17 -0800

    Revert "automate lambda URL updates in CI/CD; add terraform output and cloudflare DNS"
    This reverts commit d46597168515f9aa0e4536d3c1bfd01b30d64c23.

--- backend/provider.tf
+++ backend/provider.tf
@@ -4,21 +4,12 @@ terraform {
       source  = "hashicorp/aws"
       version = ">4.9.0"
     }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = ">3.0.0"
-    }
   }
 }
 
 provider "aws" {
   region  = "us-west-2"
-#   profile = "default"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-provider "cloudflare" {
-  email   = var.cloudflare_email
-  api_token = var.cloudflare_api_key
+  profile = "default"
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
 }
