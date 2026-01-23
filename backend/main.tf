2b35c94010523ecf74edd02a0ca721485612961f
Michael Weiser <weiserjmichael@gmail.com>
2026-01-23 12:42:59 -0800

    define aws key variables in TF

--- backend/main.tf
+++ backend/main.tf
@@ -234,6 +234,18 @@ variable "subdomain" {
   type        = string
 }
 
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
 ###################################################################################################################
 # Outputs
 
